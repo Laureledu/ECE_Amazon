a971193eed82b8409f3f78244a3081c612a6b820
Poulette <emy.mahouni@edu.ece.fr>
2019-05-03 11:01:10 +0200

    Update de la barre de navigation
    Barre nav pour admin, vendeur et acheteur

--- barre2.php
+++ barre2.php
@@ -1,108 +1,167 @@
-
-<!--<nav class="navbar navbar-inverse menu">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <a class="navbar-brand" href="index.php">ECE Amazon</a>
-    </div>
-    <ul class="nav navbar-nav">
-      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"> Catégories <span class="caret"></span></a>
-        <ul class="dropdown-menu">
-          <li><a href="#">Livres</a></li>
-          <li><a href="#">Musiques</a></li>
-          <li><a href="#">Vetements</a></li>
-          <li><a href="#">Sports et Loisirs</a></li>
-        </ul>
-      </li>
-      <li><a href="#">Ventes flash</a></li>
-    </ul>
-
-
-    <!-<- La partie de droite sera la seule à changer pour le moment
-    <?php
-      /*if(isset($_SESSION['id'])){
-
-        echo '<ul class="nav navbar-nav navbar-right droite">
-              <li><form role="form" action="traitement_deco_acheteur.php" method="post" >
-                      <button  type="submit" name = "deconnexion" class="deco btn-warning btn-sm"> Déconnexion </button>
-                      </form></li>
-              <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Panier</a></li>
-            </ul>';
-
-      }
-      else{
-
-        echo '<ul class="nav navbar-nav navbar-right droite">
-        <li><a href="connexion_acheteurs"><span class="glyphicon glyphicon-user"></span> Votre compte</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Panier</a></li>
-      </ul>';
-
-      }*/
-    ?>
-  </div>
-</nav>-->
-
-
-
-
-
-
- <!-- A horizontal navbar that becomes vertical on small screens -->
+<!-- A horizontal navbar that becomes vertical on small screens -->
       <nav class="navbar navbar-expand-sm bg-dark">
-        
-         <!-- Left -->
-         <ul class="navbar-nav mr-auto">
+        <?php
 
-            <li class="nav-item">
-               <a class="nav-link navbar-brand" href="index.php">Ece Amazon</a>
-            </li>
+          //Acheteur connecté
+          if(isset($_SESSION['id_acheteur'])){
+               //Left
+                echo '<ul class="navbar-nav mr-auto">
 
-          
-            <li class="nav-item">
-               <a class="nav-link" href="#">Vendre</a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link" href="#">Vente flash</a> 
-            </li>
+                    <li class="nav-item">
+                       <a class="nav-link navbar-brand" href="index.php">Ece Amazon</a>
+                    </li>
 
-            <li class="nav-item dropdown">
-               <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
-               Categories
-               </a>
-               <div class="dropdown-menu">
-                  <a class="dropdown-item" href="#">Livres</a>
-                  <a class="dropdown-item" href="#">Musiques</a>
-                  <a class="dropdown-item" href="#">Vetements</a>
-                  <a class="dropdown-item" href="#">Sports et Loisirs</a>
-               </div>
-            </li>
+                  
+                    <li class="nav-item">
+                       <a class="nav-link" href="#">Vendre</a>
+                    </li>
+                    <li class="nav-item">
+                       <a class="nav-link" href="#">Vente flash</a> 
+                    </li>
 
-         </ul>
+                    <li class="nav-item dropdown">
+                       <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                       Categories
+                       </a>
+                       <div class="dropdown-menu">
+                          <a class="dropdown-item" href="#">Livres</a>
+                          <a class="dropdown-item" href="#">Musiques</a>
+                          <a class="dropdown-item" href="#">Vetements</a>
+                          <a class="dropdown-item" href="#">Sports et Loisirs</a>
+                       </div>
+                    </li>
 
-          <!-- Right -->
-         <ul class="navbar-nav ml-auto">
-           <li class="nav-item">
-              <a class="nav-link" href="#">Panier</a>
-           </li>
+                  </ul>
 
+                    <!-- Right -->
+                   <ul class="navbar-nav ml-auto mr-5">
 
-           <?php
-               if(isset($_SESSION['id_acheteur'])){
-                 echo '<li class="nav-item">
-                    <form role="form" action="traitement_deco_acheteur.php" method="post" >
-                    <button  type="submit" name = "deconnexion" class="deco btn-danger btn-sm"> Déconnexion </button>
-                      </form>
-                 </li>';
-                 }else{
+                     
+                        <li class="nav-item dropdown">
+                             <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                              '.$_SESSION['pseudo_ach'].'
+                             </a>
+                               <div class="dropdown-menu">
+                                <center>
+                                  <form role="form" action="traitement_deco_acheteur.php" method="post" >
+                                    <button  type="submit" name = "deconnexion" class="deco btn-danger btn-sm"> Déconnexion </button>
+                                 </form>
+                                 </center>
+                               </div>
+                         </li>
 
-                   echo '<li class="nav-item">
-                            <a class="nav-link" href="connexion_acheteurs">Votre compte</a>
+                           <li class="nav-item">
+                              <a class="nav-link" href="#">Panier</a>
+                           </li>';
+                       } 
+
+                       //Vendeur connecté
+
+                       elseif(isset($_SESSION['id_vendeur'])){
+                     //Left
+                      echo '<ul class="navbar-nav mr-auto">
+
+                          <li class="nav-item">
+                             <a class="nav-link navbar-brand" href="index.php">Ece Amazon</a>
                           </li>
-                        <li class="nav-item">
-                          <a class="nav-link" href="#">admin</a>
-                         </li>';
 
-                 }
+                        
+                          <li class="nav-item">
+                             <a class="nav-link" href="#">Vendre</a>
+                          </li>
 
+
+                        </ul>
+
+                          <!-- Right -->
+                         <ul class="navbar-nav ml-auto mr-5">
+
+
+                            <li class="nav-item dropdown">
+                               <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                                '.$_SESSION['pseudo_du_vendeur'].'
+                               </a>
+                               <div class="dropdown-menu">
+                                  <form role="form" action="traitement_deco_acheteur.php" method="post" >
+                                    <button  type="submit" name = "deconnexion" class="deco btn-danger btn-sm"> Déconnexion </button>
+                                 </form>
+                               </div>
+                            </li>';
+                          }
+
+                      //Vendeur connecté
+
+                      elseif(isset($_SESSION['id_admin'])){
+                     //Left
+                      echo '<ul class="navbar-nav mr-auto">
+
+                          <li class="nav-item">
+                             <a class="nav-link navbar-brand" href="index.php">Ece Amazon</a>
+                          </li>
+
+       
+                        </ul>
+
+                          <!-- Right -->
+                         <ul class="navbar-nav ml-auto mr-5">
+
+
+                            <li class="nav-item dropdown">
+                               <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                                '.$_SESSION['pseudo_admin'].'
+                               </a>
+                               <div class="dropdown-menu">
+                                  <form role="form" action="traitement_deco_acheteur.php" method="post" >
+                                    <button  type="submit" name = "deconnexion" class="deco btn-danger btn-sm"> Déconnexion </button>
+                                 </form>
+                               </div>
+                            </li>';
+                          }
+
+                         else{
+                          //Left -->
+                           echo' <ul class="navbar-nav mr-auto">
+
+                              <li class="nav-item">
+                                 <a class="nav-link navbar-brand" href="index.php">Ece Amazon</a>
+                              </li>
+
+                            
+                              <li class="nav-item">
+                                 <a class="nav-link" href="#">Vendre</a>
+                              </li>
+                              <li class="nav-item">
+                                 <a class="nav-link" href="#">Vente flash</a> 
+                              </li>
+
+                              <li class="nav-item dropdown">
+                                 <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">
+                                 Categories
+                                 </a>
+                                 <div class="dropdown-menu">
+                                    <a class="dropdown-item" href="#">Livres</a>
+                                    <a class="dropdown-item" href="#">Musiques</a>
+                                    <a class="dropdown-item" href="#">Vetements</a>
+                                    <a class="dropdown-item" href="#">Sports et Loisirs</a>
+                                 </div>
+                              </li>
+
+                           </ul>
+
+                            <!-- Right -->
+                           <ul class="navbar-nav ml-auto">
+                              <li class="nav-item">
+                                <a class="nav-link" href="#">Panier</a>
+                              </li>
+
+                               <li class="nav-item">
+                                  <a class="nav-link" href="connexion_acheteurs">Votre compte</a>
+                                </li>
+                              <li class="nav-item">
+                                 <a class="nav-link" href="#">admin</a>
+                               </li>';
+
+                       }
 
            ?>
 
